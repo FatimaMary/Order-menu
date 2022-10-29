086f5708770bf54cc96d7da6a54ae3683579275c
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2022-10-29 16:11:38 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/header';
+import Menu from './components/header/menu';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
